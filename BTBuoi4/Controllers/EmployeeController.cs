c8ded900afcdcca756daffdea21ef893f6714bcf
P1emon <khang113324@gmail.com>
2024-07-19 14:28:49 +0700

    Cập nhật lại EController và View

--- BTBuoi4/Controllers/EmployeeController.cs
+++ BTBuoi4/Controllers/EmployeeController.cs
@@ -1,31 +1,41 @@
 ﻿using BTBuoi4.Models;
+using BTBuoi4.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BTBuoi4.Controllers
+namespace DemoValidation.Controllers
 {
-    public class EmployeeController : Controller
-    {
-        public IActionResult JoinWithUs()
-        {
-            return View();
-        }
-        [HttpPost]
-        public IActionResult Register(Employee emp)
-        {
-            if (!ModelState.IsValid)
-            {
-                ModelState.AddModelError("Lỗi", "Dữ liệu không hợp lệ");
-            }
-            return View();
-        }
-        public IActionResult checkExistedEmployee(string EmplyeeNo)
-        {
-            var dsNhanVien = new List<string>{"aaaaaa", "bbbbbb", "cccccc"};
-            if (dsNhanVien.Contains(EmplyeeNo))
-            {
-                return Json($"{EmplyeeNo} đã có");
-            }
-            return Json(true);
-        }   
-    }
+	public class EmployeeController : Controller
+	{
+		public string CheckExistedEmployeeEmail(string Email)
+		{
+			var emailsInDb = new List<string> { "admin@gmail.com", "hyhien@gmail.com" };
+			return emailsInDb.Contains(Email) ? "false" : "true";
+		}
+
+		public IActionResult JoinWithUs()
+		{
+			return View();
+		}
+
+		public IActionResult CheckExistedEmployee(string EmployeeNo)
+		{
+			var dsNhanVien = new List<string> { "aaaaaa", "bbbbbb", "cccccc" };
+			if (dsNhanVien.Contains(EmployeeNo)) return Json($"{EmployeeNo} đã có.");
+			return Json(true);
+		}
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Register(Employee emp)
+		{
+			if (!ModelState.IsValid)
+			{
+				ModelState.AddModelError("loi", "Còn lỗi k");
+			}
+			return View();
+		}
+	}
 }
